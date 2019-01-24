04cd810e8cc451785e95d3eb33bd503feeb07fd9
Jiang Liangdong <jld@vipmro.com>
2019-01-24 19:58:04 +0800

    Only run clean and configuring if parameter -clean is passed.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ EXE="$0"
 EXEDIR=`dirname "$EXE"`
 BASEDIR=`cd "$EXEDIR/.." && pwd -P`
 cd "$BASEDIR"
-make clean
-./configure --prefix=/usr/local/nginx-image --with-http_stub_status_module $@ --add-module=./ngx_image_thumb
+if [ "$1" == "-clean"]; then
+  make clean
+  ./configure --prefix=/usr/local/nginx-image --with-http_stub_status_module $@ --add-module=./ngx_image_thumb
+fi
 make
